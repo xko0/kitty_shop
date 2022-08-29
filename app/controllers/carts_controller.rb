e9effe0c661a5145420c56555364e35cd2e77839
xko <germain.rgb@hotmail.fr>
2022-08-29 18:48:40 +0200

    reseting cart controller + alerts

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,32 +1,13 @@
 class CartsController < ApplicationController
-  def index
-    @cart = find_cart
-  end
+  def index; end
 
-  def create
-    @cart = find_cart
-    @item = Item.find(params[:item_id])
-    check_item = Purchase.find_by(item: @item, cart: @cart)
-    if !check_item.nil?
-      flash[:error] = "This item is already added in cart."
-      return redirect_to items_path
-    end
-    @purchase = Purchase.new(cart: @cart, item: @item)
-    if @purchase.save
-      redirect_to items_path
-    end
-  end
+  def new; end
 
-  def destroy
-    @purchase = Purchase.find(params[:purchase_id])
-    if @purchase.destroy
-      flash[:success] = "Remove successfully an item"
-    else
-      flash[:error] = "An error has been occured!"
-    end
-    redirect_to carts_path
-  end
+  def show; end
+
+  def create; end
+
+  def destroy; end
 
   def update; end
-
 end
