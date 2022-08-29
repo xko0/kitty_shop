a9078c2fa64edef3c46e1c925cf521a2688b9c02
GuillaumeBrthlt <bertholetguillaume@gmail.com>
2022-08-29 15:03:56 +0200

    modified seed and added validations

--- app/models/Item.rb
+++ app/models/Item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+  validates :title, length: { minimum: 5 }
+  validates :description, length: { minimum: 50 }
+  validates :title, :description, :price, :image_url, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 5 }
 end
