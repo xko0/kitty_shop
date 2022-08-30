302327f3292973f0d4273787ecfdb18b00ef528d
xko <germain.rgb@hotmail.fr>
2022-08-30 14:08:52 +0200

    routes almost done and front for user edit

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'items#index'
-  resources :items, only: %i[index show]
-  resources :carts, only: %i[show create update destroy]
-  resources :purchases, only: %i[create destroy]
+  resources :items, only: %i[index show], :path => 'image'
+  resources :carts, only: %i[show create update destroy], :path => 'mon_panier'
+  resources :purchases, only: %i[create destroy], :path => 'achat'
 end
