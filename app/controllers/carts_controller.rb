5d18ae5679f0428cf7a283c589acf0ff1a95164d
GuillaumeBrthlt <bertholetguillaume@gmail.com>
2022-08-30 12:34:31 +0200

    impossible to go on other user's cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,14 +1,11 @@
 class CartsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:show] do
+    redirect_to root_path unless is_user_cart?
+  end
 
   def show
     @current_cart = find_cart
     @items = @current_cart.items
   end
 
-  def create; end
-
-  def destroy; end
-
-  def update; end
 end
