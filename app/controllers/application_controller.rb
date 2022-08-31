2997a54bf1611007b4bb3dd632135aa2a5ddc411
GuillaumeBrthlt <bertholetguillaume@gmail.com>
2022-08-31 15:05:01 +0200

    add all admins and creation of the dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   include CartsHelper
   include ItemsHelper
   add_flash_types :info, :error, :success, :alert
+
+  def authorize_admin
+    if user_signed_in?
+      redirect_to root_path, status: 401 unless current_user.admin
+    else
+      redirect_to root_path
+    end
+    # redirects to previous page
+  end
 end
