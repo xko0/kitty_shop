573c3634adb1b734701aff3aa3456ebac4ce4f79
Chreees79 <christelle.gevaert32@gmail.com>
2022-09-01 16:36:20 +0200

    issue with pictures attached in mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,11 +58,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  
-  config.action_controller.asset_host = 'localhost:3000'
-  config.action_mailer.asset_host = "http://localhost:3000"
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000, :protocol =>"http://" }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :mailjet
 end
